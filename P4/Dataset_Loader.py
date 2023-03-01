189fa8c79b4fd8af13ee00a6c08c8b130d7cf1ad
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-03-01 23:40:25 +0000

    Make the dataset loader now, make sure its only 1 batch

--- P4/Dataset_Loader.py
+++ P4/Dataset_Loader.py
@@ -2,10 +2,11 @@
 
 from code.base_class.dataset import dataset
 import torch
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 from pathlib import Path
 import re
 import pickle
+
 
 class Dataset_Loader(dataset):
     data = {}
@@ -45,62 +46,139 @@ class Dataset_Loader(dataset):
         BERT_MAX_LENGTH = 512
 
         for i, (text, label) in enumerate(self.processingData['train']):
-            out = tokenizer(text, padding='max_length', max_length=BERT_MAX_LENGTH, add_special_tokens=True)    
+            out = tokenizer(text, padding='max_length',
+                            max_length=BERT_MAX_LENGTH, add_special_tokens=True)
             tokenized = out['input_ids']
             tokenType = out['token_type_ids']
             attention = out['attention_mask']
             truncated = {
-                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],    # keep special start and end symbol
+                # keep special start and end symbol
+                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],
                 'token_type_ids': tokenType[0:1] + tokenType[-(BERT_MAX_LENGTH-1):],
                 'attention_mask': attention[0:1] + attention[-(BERT_MAX_LENGTH-1):],
             }
             self.inputData['train']['X'].append(tokenized)
-            self.inputData['train']['y'].append(label)           
+            self.inputData['train']['y'].append(label)
 
         for i, (text, label) in enumerate(self.processingData['test']):
-            out = tokenizer(text, padding='max_length', max_length=BERT_MAX_LENGTH, add_special_tokens=True)    
+            out = tokenizer(text, padding='max_length',
+                            max_length=BERT_MAX_LENGTH, add_special_tokens=True)
             tokenized = out['input_ids']
             tokenType = out['token_type_ids']
             attention = out['attention_mask']
             truncated = {
-                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],    # keep special start and end symbol
+                # keep special start and end symbol
+                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],
                 'token_type_ids': tokenType[0:1] + tokenType[-(BERT_MAX_LENGTH-1):],
                 'attention_mask': attention[0:1] + attention[-(BERT_MAX_LENGTH-1):],
             }
             self.inputData['test']['X'].append(tokenized)
-            self.inputData['test']['y'].append(label)             
-            
+            self.inputData['test']['y'].append(label)
+
         return self.inputData
 
-            
     def load(self, save=False) -> dict:
         # * 1 is for positive, 0 is negative
         print('loading data....')
-        dataPath = self.dataFolder
+        processedData = {
+            'train': [],
+            'test': []
+        }
+        dataPath = 'data/test_data'
 
         for p in Path(dataPath, 'train', 'pos').glob('*.txt'):  # 1 is pos and 0 is neg
             entry = p.read_text(encoding='utf8')
-            self.processingData['train'].append((entry, 1))
+            processedData['train'].append((entry, 1))
         for p in Path(dataPath, 'train', 'neg').glob('*.txt'):  # 1 is pos and 0 is neg
             entry = p.read_text(encoding='utf8')
-            self.processingData['train'].append((entry, 0))
+            processedData['train'].append((entry, 0))
 
         for p in Path(dataPath, 'test', 'pos').glob('*.txt'):  # 1 is pos and 0 is neg
             entry = p.read_text(encoding='utf8')
-            self.processingData['test'].append((entry, 1))
+            processedData['test'].append((entry, 1))
         for p in Path(dataPath, 'test', 'neg').glob('*.txt'):  # 1 is pos and 0 is neg
             entry = p.read_text(encoding='utf8')
-            self.processingData['test'].append((entry, 0))
+            processedData['test'].append((entry, 0))
 
         # Clean the training data
-        self.processingData['train'] = self.cleanTrainData()
-        # Convert data to tensor
-        self.encodeForRNN()
+        cleanedData = []
+        CLEANHTML = re.compile('<.*?>')
+
+        for entry in processedData['train']:
+            # ! Hope the data doesn't contain heavy html tags or else it wouldn't work
+            text = re.sub(CLEANHTML, '', entry[0])
+            cleanedData.append((text, entry[1]))
+
+        # Tokenizing the data
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        BERT_MAX_LENGTH = 512
+        inputData = {
+            'train': {
+                'X': [],
+                'y': []
+            },
+            'test': {
+                'X': [],
+                'y': []
+            }
+        }
+
+        for i, (text, label) in enumerate(processedData['train']):
+            out = tokenizer(text, padding='max_length',
+                            max_length=BERT_MAX_LENGTH, add_special_tokens=True)
+            tokenized = out['input_ids']
+            tokenType = out['token_type_ids']
+            attention = out['attention_mask']
+            truncated = {
+                # keep special start and end symbol
+                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],
+                'token_type_ids': tokenType[0:1] + tokenType[-(BERT_MAX_LENGTH-1):],
+                'attention_mask': attention[0:1] + attention[-(BERT_MAX_LENGTH-1):],
+            }
+            inputData['train']['X'].append(truncated)
+            inputData['train']['y'].append(label)
+
+        for i, (text, label) in enumerate(processedData['test']):
+            out = tokenizer(text, padding='max_length',
+                            max_length=BERT_MAX_LENGTH, add_special_tokens=True)
+            tokenized = out['input_ids']
+            tokenType = out['token_type_ids']
+            attention = out['attention_mask']
+            truncated = {
+                # keep special start and end symbol
+                'input_ids': tokenized[0:1] + tokenized[-(BERT_MAX_LENGTH-1):],
+                'token_type_ids': tokenType[0:1] + tokenType[-(BERT_MAX_LENGTH-1):],
+                'attention_mask': attention[0:1] + attention[-(BERT_MAX_LENGTH-1):],
+            }
+            inputData['test']['X'].append(truncated)
+            inputData['test']['y'].append(label)
+
+            # Load the BERT embedding model
+            bertModel = BertModel.from_pretrained('bert-base-uncased', output_hidden_states=True)
+            bertModel.eval()    # Only wants to use the bert model
+
+            # Convert inputs to pytorch tensor
+            tokens_list = []
+            for each in inputData['train']['X']:
+                tokens_list.append(each['input_ids'])
+            tokens_tensor = torch.tensor(tokens_list)
+
+            # BERT is trained and expect sentence pairs, so we need to number each tensor to belong to a text
+            segments_ids = [[x] * 512 for x in range(0, len(inputData['train']['X']))]
+            segments_tensor = torch.tensor(segments_ids)
+
+            with torch.no_grad():
+                outputs = bertModel(tokens_tensor, segments_tensor)
+                hidden_states = outputs[2]
+            # Cut the layer to feed to RNN
+            stackedHStates = torch.stack(hidden_states, dim=0)[-1]
+            # ! IN Data loader you load 1 by 1 => batch size of 1, make it work
 
         # Save the conversion
         if save:
             with open(f'P4/saved/{self.task}-dataInTensor', 'wb') as handle:
-                pickle.dump(self.inputData, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(self.inputData, handle,
+                            protocol=pickle.HIGHEST_PROTOCOL)
 
         return self.inputData
 
