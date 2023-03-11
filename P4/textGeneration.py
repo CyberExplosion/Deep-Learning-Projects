e43d4a1f9e6f1d57c52546bacae367dd0067f427
Khoi Nguyen <koinguyen@ucdavis.edu>
2023-03-11 22:20:32 +0000

    First commit for Phase 5

--- P4/textGeneration.py
+++
@@ -1,7 +0,0 @@
-from Settings import Settings
-import matplotlib.pyplot as plt
-
-
-t = Settings(sRandSeed=31, sTask="text_generation", sUseSave=True)
-res = t.load_run_save_evaluate()
-print(f'Accurarcy is: {res * 100}%')
